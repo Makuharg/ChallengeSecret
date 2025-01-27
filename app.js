49ed642acd23564208d74ed5fc7f1e86ddd7378e
Marcos Gonzalvez <marcosgondesign@gmail.com>
2025-01-27 19:21:25 -0300

    generando funcion sortearAmigo() y terminando desafio

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ let amigos = [];
 
 function agregarAmigo(){
     // conseguimos el valor del input
-    const value = document.getElementById('amigo').value;
-
+    let value = document.getElementById('amigo').value;
     // condicional, si value es un string vacio tira un alert
     if(value){
         let listaAmigos = document.getElementById('listaAmigos');
@@ -20,4 +19,24 @@ function agregarAmigo(){
     amigos.push(value);
     // Un poco de UX para que el usuario no tenga que estar borrando el nombre a mano
     document.getElementById('amigo').value = "";
+};
+
+
+function sortearAmigo(){
+    // establecemos numero maximo para que no elijamos un nombre que no se encuentre en el array
+    let numeroMaximo = amigos.length
+    // generamos numero aleatorio 
+    let numeroAleatorio = Math.floor(Math.random()*numeroMaximo)+1
+
+    if(numeroAleatorio){
+        // en este caso hay que restarle 1 al numeroAleatorio ya que el indice de mi array empieza en 0
+        let amigoGanador = amigos[numeroAleatorio-1];
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<li>¡Felicidades ${amigoGanador} has ganado el sorteo del amigo secreto!</li>`;
+    } else{
+        alert("Por favor ingrese la lista de amigos")
+    };
+    // borramos lista de amigos para que quede solo el ganador
+    document.getElementById('listaAmigos').innerHTML = "";
 };
